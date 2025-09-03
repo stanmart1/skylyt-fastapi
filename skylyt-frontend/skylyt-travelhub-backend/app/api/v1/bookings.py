81b9cdd44e038abd808770651ce45f0de1a02187
stanmart1 <scaleitpro@gmail.com>
2025-09-03 10:07:43 +0100

    fixed import error

--- skylyt-frontend/skylyt-travelhub-backend/app/api/v1/bookings.py
+++ skylyt-frontend/skylyt-travelhub-backend/app/api/v1/bookings.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from typing import List
 from app.core.database import get_db
